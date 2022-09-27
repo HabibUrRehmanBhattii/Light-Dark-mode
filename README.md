6f4a5634f871f621e55e0900b0048a94699157a2
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2022-09-27 03:32:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dark & Light Mode Website
+ In this project, I have created a website that has a dark and light mode. I have used HTML, CSS, and JavaScript to create this website. I have used the `localStorage` to store the theme of the website. I have also used the `toggle` function to switch between the dark and light mode. It's not responsive yet, but I will make it responsive in the future. 
+ ## Screenshots
+### Dark Mode
+![Dark Mode](
+### Light Mode
+![Light Mode](
+## Live Demo
+[Live Demo Link]()
+## Built With
+- HTML
+- CSS
+- JavaScript
+## Getting Started
+To get a local copy up and running follow thesesimple example steps.
+### Prerequisites
+- A web browser
+### Setup
+ - Clone the repository
+ ### Install
+  - Open the `index.html` file in your browser
+    ### Usage
+    - You can use this website to switch between dark andlight mode
+### Run tests
+- Open the `index.html` file in your browser
+### Deployment
+- Clone the repository
+### Authors
+👤 **Bhatti**
+- GitHub: [@Bhatti](https://github.com/HabibUrRehmanBhattii)
+- Xing: [@Bhatti](https://www.xing.com/profile/HabibUrRehman_BHatti/cv)
+- LinkedIn: [@Bhatti](https://www.linkedin.com/in/habib-ur-rehman-bhatti-884713103/)
+
+### 🤝 Contributing
+    Contributions, issues, and feature requests are welcome!
+### Show your support
+    Give a ⭐️ if you like this project!
+### Acknowledgments
+- [Microverse](https://www.microverse.org/)
+- [The Odin Project](https://www.theodinproject.com/)
+### 📝 License
+No license required for this project. Feel free to use it. 
---
+++ script.js
@@ -0,0 +1,69 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');//' is used to select the element and " is used to select the attribute
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+
+// Dark or Light Mode Images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+};
+
+
+
+// Dark Mode Settings
+function darkMode(){
+nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+toggleIcon.children[0].textContent = "Dark Mode";
+toggleIcon.children[1].classList.replace("fa-sun", "fa-moon"); 
+imageMode("dark");
+
+}
+
+// Light Mode Settings
+function lightMode(){
+nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+toggleIcon.children[0].textContent = "Light Mode";
+toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+image1.src = "img/undraw_proud_coder_light.svg";
+image2.src = "img/undraw_feeling_proud_light.svg";
+image3.src = "img/undraw_conceptual_idea_light.svg";
+imageMode("light");
+
+}
+
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if(event.target.checked){
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+        darkMode();
+    }else{
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+        lightMode();
+    }
+}
+
+
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Checking Local Stroage
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+        darkMode();
+    };  
+};
